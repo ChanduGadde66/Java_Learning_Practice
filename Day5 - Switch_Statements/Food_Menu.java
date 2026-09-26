94ff8b8837aae5f3c22c28aad9a45a70a770ac6b
Your Name <your@email.com>
2026-09-26 15:16:05 +0530

    Switch Practical Learning

--- Day5 - Switch_Statements/Food_Menu.java
+++ Day5 - Switch_Statements/Food_Menu.java
@@ -1,4 +1,51 @@
-package PACKAGE_NAME;
+import java.util.*;
 
-public class Food_Menu {
+class Food_Menu {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1. Idly * 4 (1 plate)  $1");
+        System.out.println("2. Dosa * 1            $3");
+        System.out.println("3. Biryani * 1         $15");
+        System.out.println("4. Fried Rice * 1      $10");
+        int ch;
+        System.out.println("Enter Choice : ");
+        ch = sc.nextInt();
+
+        int quantity;
+        int Idly_price = 1;
+        int dosa_price = 3;
+        int biryani_price = 15;
+        int friedrice_price = 10;
+        int total_price;
+
+        switch(ch) {
+            case 1 :
+                     System.out.println("Enter Quantity of idly in plates : ");
+                     quantity = sc.nextInt();
+                     total_price = quantity * Idly_price;
+                     System.out.println("The total price for idly is : " + total_price + "$");
+                     break;
+            case 2 :
+                System.out.println("Enter Quantity of dosa in plates : ");
+                quantity = sc.nextInt();
+                total_price = quantity * dosa_price;
+                System.out.println("The total price for dosa is : " + total_price + "$");
+                break;
+            case 3 :
+                System.out.println("Enter Quantity of Biryani in plates : ");
+                quantity = sc.nextInt();
+                total_price = quantity * biryani_price;
+                System.out.println("The total price for Biryani is : " + total_price + "$");
+                break;
+            case 4 :
+                System.out.println("Enter Quantity of Fried rice in plates : ");
+                quantity = sc.nextInt();
+                total_price = quantity * friedrice_price;
+                System.out.println("The total price for idly is : " + total_price + "$");
+                break;
+            default :
+                System.out.println("The food item is not available now...");
+        }
+
+    }
 }
--- Day5 - Switch_Statements/Quarters.java
+++ Day5 - Switch_Statements/Quarters.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class Quarters {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ch;
+        System.out.println("Enter choice : ");
+        ch = sc.nextInt();
+        switch(ch) {
+            case 1 :
+            case 2 :
+            case 3 :
+                System.out.println("Quarter 1 ");
+                break;
+            case 4 :
+            case 5 :
+            case 6 :
+                System.out.println("Quarter 2 ");
+                break;
+            case 7 :
+            case 8 :
+            case 9 :
+                System.out.println("Quarter 3 ");
+                break;
+            case 10 :
+            case 11 :
+            case 12 :
+                System.out.println("Quarter 4 ");
+                break;
+            default :
+                System.out.println("Enter in range 1 - 12 only....");
+        }
+    }
 }
